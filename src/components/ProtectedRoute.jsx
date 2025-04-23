f994c3003b394e28a763550108876259506bec5a
Dylan Guzman <70229022+dgoose2@users.noreply.github.com>
2025-04-22 22:27:21 -0700

    adding protection

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { useRouter } from "next/navigation";
+
+export default function ProtectedRoute({ children }) {
+  const router = useRouter();
+  const [checkingAuth, setCheckingAuth] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.push("/login");
+      } else {
+        setCheckingAuth(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (checkingAuth) return null; // or show a loading spinner
+  return children;
+}
